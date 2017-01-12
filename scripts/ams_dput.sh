9bcc71cf535b0e5133d93891dc02063ee96538d8
Toby Schneider <toby@gobysoft.org>
2017-01-12 16:29:40 -0500

    Switching to sbuild from pbuilder-dist

---
+++ scripts/ams_dput.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+pushd /home/ubuntu/ubuntu/build >& /dev/null
+for i in *.changes; do
+    j=$(echo $i | sed s/.changes//);
+    if [ ! -e $j.local.upload ]; then 
+        echo "dput $i"
+        dput local $i
+    fi;
+done
+popd >& /dev/null
+
+mini-dinstall -b
